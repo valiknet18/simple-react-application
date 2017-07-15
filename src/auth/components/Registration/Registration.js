089b05757474432428254af0db241ab326d7cf1d
Valentyn Hrynevych <valik.v1per@gmail.com>
2017-07-15 14:15:15 +0300

    Added registration form

--- src/auth/components/Registration/Registration.js
+++ src/auth/components/Registration/Registration.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
 import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class Registration extends Component {
     render() {
         return (
-            <Paper>
-                Registration form
+            <Paper className="Login-container">
+                <div>
+                    <h3>Форма регистрации</h3>
+                </div>
+                <div>
+                    <div>
+                        <TextField
+                            className="Contact-fields"
+                            floatingLabelText="Ваше имя"
+                            type="text"
+                        />
+                    </div>
+                    <div>
+                        <TextField
+                            className="Contact-fields"
+                            floatingLabelText="Ваш username"
+                            type="text"
+                        />
+                    </div>
+                    <div>
+                        <TextField
+                            className="Contact-fields"
+                            floatingLabelText="Ваш email"
+                            type="email"
+                        />
+                    </div>
+                    <div>
+                        <TextField
+                            className="Contact-fields"
+                            floatingLabelText="Ваш password"
+                            type="password"
+                        />
+                    </div>
+                </div>
+                <div>
+                    <RaisedButton label="Регистрация" fullWidth={true} />
+                </div>
             </Paper>
         );
     }
